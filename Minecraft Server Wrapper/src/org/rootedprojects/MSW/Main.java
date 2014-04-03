46c753f7190247f91f162fe8704eb7a5ec557fcf
RootedMaster <RootedMaster@users.noreply.github.com>
2014-04-03 17:11:57 +0200

    Main File

---
+++ Minecraft Server Wrapper/src/org/rootedprojects/MSW/Main.java
@@ -0,0 +1,10 @@
+package org.rootedprojects.MSW;
+
+/**
+ * Created by Hans Goor on 3-4-2014.
+ * CopyRight Rooted Inc.
+ * DO NOT EDIT THIS CODE WITHOUT PERMISSION OF ROOTED INC.
+ */
+
+public class Main {
+}
